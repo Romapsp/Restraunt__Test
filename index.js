fe49739bc213abcf15b53bb292e4b0c2ac37b3f6
Roman <surkov.ron@gmail.com>
2022-02-21 13:03:21 +0300

    2nd

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const modalBtn = document.querySelector(".header__info_btn");
 const modalWindow = document.querySelector('.modal-window');
 const closeBtn = document.querySelector (".modal-window");
 const bannerBtn = document.querySelector (".banner__btn_menu");
+const celebrateBtn = document.querySelector(".order__btn");
 
 modalBtn.addEventListener("click", () => {
  modalWindow.classList.add("modal-window__show");
@@ -16,6 +17,15 @@ modalBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
     modalWindow.classList.remove("modal-window__show");
 });
+
+celebrateBtn.addEventListener("click", () => {
+    modalWindow.classList.add("modal-window__show");
+   });
+   
+   
+   closeBtn.addEventListener("click", () => {
+       modalWindow.classList.remove("modal-window__show");
+   });
 
 bannerBtn.addEventListener("click", () => {
     window.location.replace('/#menu');
