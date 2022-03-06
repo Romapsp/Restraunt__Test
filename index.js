693d03df8cd5099efd620ac6240a15194bf21d65
Roman <surkov.ron@gmail.com>
2022-03-07 02:39:15 +0300

    3rd

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 
 
-const modalBtn = document.querySelector(".header__info_btn");
+const modalBtn = document.querySelector(".header__order-btn");
 const modalWindow = document.querySelector('.modal-window');
 const closeBtn = document.querySelector (".modal-window");
 const bannerBtn = document.querySelector (".banner__btn_menu");
@@ -29,4 +29,46 @@ celebrateBtn.addEventListener("click", () => {
 
 bannerBtn.addEventListener("click", () => {
     window.location.replace('/#menu');
+});
+///////////////////////////////////////////////////////
+const header = document.querySelector (".header");
+const links = document.querySelectorAll (".header__link");
+const info = document.querySelector (".header__info");
+const phone = document.querySelector (".header__phone-number");
+const cart = document.querySelector (".header__cart-icon");
+const phoneText = document.querySelector (".header__phone");
+
+document.addEventListener("scroll", () => {
+    if (window.pageYOffset !== 0) {
+        header.classList.add("header_white");
+        links.forEach(item => {
+            item.classList.add("header__link_white");
+        });
+        info.classList.add("header__info_white");
+        phone.classList.add("header__phone-number_white");
+        cart.classList.add("header__cart-icon_white");
+        phoneText.classList.add("header__phone_white");
+    } else {
+        header.classList.remove("header_white");
+        links.forEach(item => {
+            item.classList.remove("header__link_white");
+        });
+        info.classList.remove("header__info_white");
+        phone.classList.remove("header__phone-number_white");
+        cart.classList.remove("header__cart-icon_white");
+        phoneText.classList.remove("header__phone_white");
+
+    }
+});
+//////////////////////////////////////////////////////////
+const imgBig = document.querySelectorAll(".meals__img-big");
+const imgSmall = document.querySelectorAll(".meals__img-small");
+
+imgSmall.forEach((small, index) => {
+    small.addEventListener("click", () => {
+        imgBig.forEach(big => {
+            big.classList.remove("meals__img-big_active");
+        });
+        imgBig[index].classList.add("meals__img-big_active");
+    });
 });
